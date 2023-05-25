0c5164d0e1c1e5a3abd9ccb418732b133f750ab4
Jayson Galban <100999803+JeeSenpai@users.noreply.github.com>
2023-05-25 12:02:00 +0800

    Update cases.service.ts

--- backend/src/cases/cases.service.ts
+++ backend/src/cases/cases.service.ts
@@ -207,7 +207,7 @@ export class CasesService {
   }
 
   async deleteAllCases(){
-    await this.caseRepository.createQueryBuilder('case').delete().execute();
+    return await this.caseRepository.createQueryBuilder().delete().execute();
   }
 
 }
