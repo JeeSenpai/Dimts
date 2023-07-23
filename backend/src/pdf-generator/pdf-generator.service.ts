ae32a5396ed86934a821a3f332a24955c466b6f6
Jayson Galban <100999803+JeeSenpai@users.noreply.github.com>
2023-07-23 23:24:00 +0800

    updates

--- backend/src/pdf-generator/pdf-generator.service.ts
+++ backend/src/pdf-generator/pdf-generator.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import puppeteer from 'puppeteer';
 const hbs = require('handlebars');
 const path = require('path');
+const puppeteer = require('puppeteer');
 const fs = require('fs-extra');
-import moment from 'moment';
+const moment = require('moment');
 
 hbs.registerHelper("dateFormat", function (value) {
   if(value == 'N/A'){
@@ -26,7 +26,7 @@ hbs.registerHelper("mname", function(value, options)
 @Injectable()
 export class PdfGeneratorService {
 
-    async compile(templatename: any, data: any) {
+    async compile(templatename, data) {
         const filepath = path.join(process.cwd(), 'src/pdf-generator/template', `${templatename}.hbs`);
         //const filepath = path.join(process.cwd(), 'dist/pdf-generator/template', `${templatename}.hbs`);
         const html = await fs.readFile(filepath, 'utf-8');
@@ -34,7 +34,7 @@ export class PdfGeneratorService {
         return compile
     }
 
-    async printReport(data: any): Promise<Buffer>{
+    async printReport(data: any){
 
         const datas = [{
             institute: 'qwrwqr',
@@ -43,8 +43,10 @@ export class PdfGeneratorService {
         try {
           const browser = await puppeteer.launch({ headless: true });
           const page = await browser.newPage();
+
           const content = await this.compile("pdf-file", data); // Use 'datas' instead of 'data'
           await page.setContent(content);
+
           const buffer = await page.pdf({
             format: 'A4',
             margin: {
@@ -57,6 +59,7 @@ export class PdfGeneratorService {
           });
           await browser.close();
           return buffer;
+
         } catch (e) {
           console.log(e);
         }
